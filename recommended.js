d285df55a48ee30c4d86b8652407fb7175cf5219
Daniel Imhoff <dwieeb@gmail.com>
2020-09-15 13:52:26 -0700

    feat: enable import/no-duplicates

--- recommended.js
+++ recommended.js
@@ -21,6 +21,7 @@ module.exports = {
     'import/first': 'error',
     'import/order': ['error', { 'alphabetize': { 'order': 'asc', 'caseInsensitive': false }, 'groups': [['builtin', 'external'], 'parent', ['sibling', 'index']], 'newlines-between': 'always' }],
     'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
     'import/no-mutable-exports': 'error',
   },
 };
