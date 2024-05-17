a78af056fb1b6c8c23ebd9f6645610fc48298ca9
jcesarmobile <jcesarmobile@gmail.com>
2024-05-17 16:43:12 +0200

    feat: Match capacitor eslint

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint', 'import'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint',
+    'plugin:import/typescript',
   ],
   rules: {
     // https://eslint.org/docs/rules/
@@ -17,6 +15,30 @@ module.exports = {
     // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin/docs/rules
     '@typescript-eslint/no-this-alias': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': ['warn', { 'allowArgumentsExplicitlyTypedAsAny': true }],
+    '@typescript-eslint/explicit-module-boundary-types': [
+      'error',
+      { allowArgumentsExplicitlyTypedAsAny: true },
+    ],
+
+    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin/docs/rules
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/consistent-type-assertions': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/prefer-for-of': 'error',
+    '@typescript-eslint/prefer-optional-chain': 'error',
+
+    // https://github.com/benmosher/eslint-plugin-import
+    'import/first': 'error',
+    'import/order': [
+      'error',
+      {
+        'alphabetize': { order: 'asc', caseInsensitive: false },
+        'groups': [['builtin', 'external'], 'parent', ['sibling', 'index']],
+        'newlines-between': 'always',
+      },
+    ],
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-mutable-exports': 'error',
   },
 };
